247dae3758e01c922b731cc0516ba4a61960c3c4
datadm <dan.mason@bthft.nhs.uk>
2020-06-26 11:44:28 +0100

    Build chart data

--- R/run_all_child_reports.R
+++ R/run_all_child_reports.R
@@ -10,13 +10,6 @@ chart_settings_file <- "R/chart_settings.csv"
 cohort_country_file <-  "R/cohort_country.csv"
 cohort_language_file <- "R/cohort_language.csv"
 
-# read and filter data by cohort
-all_data <- read_csv(cfg_data_file, col_types = "ccnnnnnnnnnnn")
-cohort_data <- all_data %>% filter(hs_cohort == cfg_cohort)
-
-# apply child filter if there is one
-if(!is.null(cfg_child_filter)) cohort_data <- cohort_data %>% filter(HelixID %in% cfg_child_filter)
-
 # read chart settings with cohort language filter
 chart_settings <- read_csv(chart_settings_file)
 cohort_language <- read_csv(cohort_language_file)
@@ -31,6 +24,20 @@ cohort_country <- cohort_country %>% filter(lan == output_language)
 # merge chart settings with language
 chart_settings <- chart_settings %>% inner_join(chart_labels)
 
+# read and filter data by cohort
+all_data <- read_csv(cfg_data_file, col_types = "ccnnnnnnnnnnn")
+
+# add country information
+all_data <- all_data %>% left_join(cohort_country)
+
+# make change to sedentary hours
+all_data <- all_data %>% mutate(sed_hr = hs_sd_wk/60)
+
+# get cohort children
+cohort_children <- all_data %>% filter(hs_cohort == cfg_cohort) %>% select(HelixID)
+# apply child filter if there is one
+if(!is.null(cfg_child_filter)) cohort_children <- cohort_children %>% filter(HelixID %in% cfg_child_filter)
+
 # loop through charts
 c <- 1
 
@@ -40,6 +47,15 @@ c <- 1
 # make chart data
 # one column for each exposure
 # one row for each country plus one for all
+all_data_summary <- all_data %>% 
+  group_by(country) %>% 
+  summarise_at(chart_settings$var, mean, na.rm = TRUE) %>%
+  ungroup()
+marginal_means <- all_data_summary %>% 
+  summarise_at(chart_settings$var, mean, na.rm = TRUE) %>%
+  mutate(country = "All")
+all_data_summary <- bind_rows(all_data_summary, marginal_means)
+
 
 # make single plot
 
